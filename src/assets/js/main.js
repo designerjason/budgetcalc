fae9bc62105a194713d6bdcc40a9f7aec615476a
Jason Mc <info@jmc-website-design.co.uk>
2024-01-18 18:08:52 +0000

    fixes some recursion issue with total

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,68 +1,66 @@
-import draggable from 'vuedraggable'
+import draggable from 'vuedraggable';
 
 const App = {
-    data() {
-      return {
-        title: '',
-        cost: 0,
-        drag: false,
-        total: 0,
-        Items: [],
-        showErrors: false,
-        saveAlert: false
-      }
+  data() {
+    return {
+      title: '',
+      cost: 0,
+      drag: false,
+      Items: [],
+      showErrors: false,
+      saveAlert: false
+    };
+  },
+
+  mounted() {
+    // use local storage if set, empty if not
+    this.Items = localStorage.getItem("budgetList") ? JSON.parse(localStorage.getItem("budgetList")) : [];
+  },
+
+  methods: {
+    // add an item to the list
+    addItem() {
+      this.Items.push({
+        "title": "",
+        "cost": null
+      });
     },
 
-    mounted() {
-      // use localstorage if set, empty if not
-      this.Items = (localStorage.getItem("budgetList")? JSON.parse(localStorage.getItem("budgetList")) : [] )
+    // remove an item from the list
+    removeItem(index) {
+      this.Items.splice(index, 1);
+      this.listEmpty = this.Items.length === 0;
     },
 
-    methods: {
-      // add from the list
-      addItem() {
-          this.Items.push(
-            { 
-              "title" : "",
-              "cost" : null
-            }        
-          )
-      },
-      
-      // remove an item from the list
-      removeItem(index) {
-        this.Items.splice(index, 1);
-        this.listEmpty = this.Items.length? false : true;
-      },
+    // save the current budget list
+    saveBudget() {
+      localStorage.setItem("budgetList", JSON.stringify(this.Items));
+      this.saveAlert = true;
 
-      // save the current budget list
-      saveBudget() {
-        localStorage.setItem("budgetList", JSON.stringify(this.Items));
-        this.saveAlert = true;
+      setTimeout(() => {
+        this.saveAlert = false;
+      }, 1000);
+    }
+  },
 
-        setTimeout(() => {
-          this.saveAlert = false;
-        }, 1000);
-      }
-    },
-    
-    computed: {
-       calcTotal() {
-        const decimalPlaces = 2;
-        this.total = 0;
-        
-        this.Items.forEach( e => {
-          if(typeof e.cost === 'number') {
-            this.total += parseFloat(e.cost)
-          }
-        });
+  computed: {
+    calcTotal() {
+      const decimalPlaces = 2;
 
-        return Number(Math.round(parseFloat(this.total + 'e' + decimalPlaces)) + 'e-' + decimalPlaces).toFixed(decimalPlaces);
-      }
+      let total = 0;
+
+      this.Items.forEach((item) => {
+        if (typeof item.cost === 'number') {
+          total += parseFloat(item.cost);
+        }
+      });
+
+      return total.toFixed(decimalPlaces);
     }
   }
-  
+};
+
 const app = Vue.createApp(App);
 
-  app.component('draggable', draggable);
-  app.mount('#app');
+app.component('draggable', draggable);
+app.mount('#app');
